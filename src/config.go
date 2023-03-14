28067fde56c6e731edb03430476a59e6e101e8f3
Sagleft <sagleft@inbox.ru>
2023-03-14 10:36:05 +0300

    add env struct parse support

--- src/config.go
+++ src/config.go
@@ -8,8 +8,18 @@ import (
 
 func parseConfig() (solutionConfig, error) {
 	var cfg solutionConfig
-	if err := envconfig.Process("", &cfg); err != nil {
-		return cfg, fmt.Errorf("parse envs: %w", err)
+
+	if err := envconfig.Process("", &cfg.Main); err != nil {
+		return cfg, fmt.Errorf("parse main envs: %w", err)
 	}
+
+	if err := envconfig.Process("", &cfg.Reddit); err != nil {
+		return cfg, fmt.Errorf("parse reddit envs: %w", err)
+	}
+
+	if err := envconfig.Process("", &cfg.Utopia); err != nil {
+		return cfg, fmt.Errorf("parse utopia envs: %w", err)
+	}
+
 	return cfg, nil
 }
