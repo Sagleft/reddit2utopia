e57e9633f95c0ebf93abd2d2ca7518edd203284f
Sagleft <sagleft@inbox.ru>
2023-03-14 17:21:54 +0300

    move config parser

--- src/helpers.go
+++ src/helpers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"strings"
 	"unicode"
+
+	"github.com/kelseyhightower/envconfig"
 )
 
 func parseCronSpec(spec string) string {
@@ -19,4 +22,22 @@ func getRedditURL(url string) string {
 	}
 
 	return redditHost + url
+}
+
+func parseConfig() (solutionConfig, error) {
+	var cfg solutionConfig
+
+	if err := envconfig.Process("", &cfg.Main); err != nil {
+		return cfg, fmt.Errorf("parse main envs: %w", err)
+	}
+
+	if err := envconfig.Process("", &cfg.Reddit); err != nil {
+		return cfg, fmt.Errorf("parse reddit envs: %w", err)
+	}
+
+	if err := envconfig.Process("", &cfg.Utopia); err != nil {
+		return cfg, fmt.Errorf("parse utopia envs: %w", err)
+	}
+
+	return cfg, nil
 }
